af1224652d85bd14c6142ebcf5949740fe1a1abe
Rahulkaushik01 <kaushikrahul725@gmail.com>
2025-01-23 22:20:53 +0530

    Add files via upload

---
+++ file1/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int x;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter yout age : ");
+        x = sc.nextInt();
+        System.out.println("You are " +x + " Years old!");
+
+    }
+    }
---
+++ file1/hello.java
@@ -0,0 +1,17 @@
+import java.util.*;
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+
+        System.out.println("additon : "+ (num1 +num2));
+        System.out.println("Multiplication : "+(num1 *num2));
+        System.out.println("Divide : "+(num1 /num2));
+        System.out.println("Subtraction : "+ (num1 -num2));
+
+
+    }
+}
